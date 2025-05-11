1dfc6adaa0e3ec6f63f90cbfb75d35153335ffe3
joelsernamoreno <joelsernamoreno@gmail.com>
2025-05-11 17:46:40 +0200

    v1.0

---
+++ ESP32Sx-DetectOS/ESP32Sx-DetectOS.ino
@@ -0,0 +1,159 @@
+#include <USB.h>
+#include <USBHIDKeyboard.h>
+
+#define KEY_NUM_LOCK     0xDB
+#define KEY_CAPS_LOCK    0xC1
+#define KEY_SCROLL_LOCK  0xCF
+
+USBHIDKeyboard Keyboard;
+USBCDC SerialUSB;
+
+volatile bool led_response_received = false;
+volatile uint8_t caps_status = 0;
+volatile uint8_t num_status = 0;
+volatile uint8_t scroll_status = 0;
+volatile int led_event_count = 0;
+
+enum HostOS {
+  OS_UNKNOWN,
+  OS_WINDOWS,
+  OS_LINUX,
+  OS_MACOS
+};
+
+HostOS detected_os = OS_UNKNOWN;
+
+void usbEventCallback(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+  if (event_base == ARDUINO_USB_HID_KEYBOARD_EVENTS) {
+    arduino_usb_hid_keyboard_event_data_t* data =
+      (arduino_usb_hid_keyboard_event_data_t*)event_data;
+
+    if (event_id == ARDUINO_USB_HID_KEYBOARD_LED_EVENT) {
+      led_response_received = true;
+      led_event_count++;
+      caps_status = data->capslock;
+      num_status = data->numlock;
+      scroll_status = data->scrolllock;
+
+      SerialUSB.printf("LED event %d: Caps:%u Num:%u Scroll:%u\n", led_event_count, caps_status, num_status, scroll_status);
+    }
+  }
+}
+
+void toggleKey(uint8_t key) {
+  Keyboard.press(key);
+  delay(300);
+  Keyboard.release(key);
+  delay(500);
+}
+
+void resetKeyboardLEDs() {
+  SerialUSB.println("Resetting keyboard LEDs...");
+  led_response_received = false;
+  toggleKey(KEY_CAPS_LOCK);
+  delay(500);
+  if (caps_status) toggleKey(KEY_CAPS_LOCK);
+
+  led_response_received = false;
+  toggleKey(KEY_NUM_LOCK);
+  delay(500);
+  if (num_status) toggleKey(KEY_NUM_LOCK);
+
+  led_response_received = false;
+  toggleKey(KEY_SCROLL_LOCK);
+  delay(500);
+  if (scroll_status) toggleKey(KEY_SCROLL_LOCK);
+
+  SerialUSB.printf("LEDs after reset: Caps:%u Num:%u Scroll:%u\n",
+                  caps_status, num_status, scroll_status);
+}
+
+void detectHostOS() {
+  SerialUSB.println("=== Starting OS detection ===");
+
+  led_event_count = 0;
+  led_response_received = false;
+  toggleKey(KEY_CAPS_LOCK);
+  delay(800);
+
+  if (!led_response_received) {
+    SerialUSB.println("No LED response to CAPSLOCK");
+    toggleKey(KEY_CAPS_LOCK);
+    delay(800);
+
+    if (!led_response_received) {
+      SerialUSB.println("Still no response. Predicting macOS");
+      detected_os = OS_MACOS;
+      SerialUSB.println("Prediction: macOS");
+      return;
+    }
+  }
+
+  SerialUSB.println("Received LED response to CAPSLOCK");
+
+  bool caps_on_initial = caps_status;
+
+  led_response_received = false;
+  toggleKey(KEY_NUM_LOCK);
+  delay(800);
+  bool num_response = led_response_received;
+
+  led_response_received = false;
+  toggleKey(KEY_SCROLL_LOCK);
+  delay(800);
+  bool scroll_response = led_response_received;
+
+  SerialUSB.printf("Event count: %d\n", led_event_count);
+
+  if (led_event_count >= 3) {
+    detected_os = OS_WINDOWS;
+    SerialUSB.println("Prediction: Windows (multiple LED events)");
+  }
+  else if (caps_on_initial && (!num_response || !scroll_response)) {
+    detected_os = OS_LINUX;
+    SerialUSB.println("Prediction: Linux (partial LED response)");
+  }
+  else {
+    detected_os = OS_UNKNOWN;
+    SerialUSB.println("Prediction: Unknown OS");
+  }
+}
+
+void setup() {
+  USB.onEvent(usbEventCallback);
+  Keyboard.onEvent(usbEventCallback);
+  SerialUSB.onEvent(usbEventCallback);
+
+  USB.begin();
+  Keyboard.begin();
+  SerialUSB.begin();
+
+  delay(2000);
+  SerialUSB.println("=== USB HID OS Detector ===");
+
+  resetKeyboardLEDs();
+  detectHostOS();
+  resetKeyboardLEDs();
+}
+
+void loop() {
+  switch (detected_os) {
+    case OS_WINDOWS:
+      SerialUSB.println("Detected: Windows");
+      Keyboard.println("Windows detected");
+      break;
+    case OS_LINUX:
+      SerialUSB.println("Detected: Linux");
+      Keyboard.println("Linux detected");
+      break;
+    case OS_MACOS:
+      SerialUSB.println("Detected: macOS");
+      Keyboard.println("macOS detected");
+      break;
+    default:
+      SerialUSB.println("OS Unknown");
+      Keyboard.println("OS Unknown");
+      break;
+  }
+  delay(5000);
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ESP32Sx-DetectOS
+
+**Idea, development and implementation:** Joel Serna (@JoelSernaMoreno).
+
+Simple USB OS detector for ESP32-S2 and ESP32-S3 (BETA).
+
+Requirements:
+
+* Arduino IDE 2.3.3 or later
+* esp32 by Espressif system version v2.0.13 (Possibly, this does not work with the latest versions)
+
+Tested systems:
+
+* Ubuntu 22.04 (Linux detected)
+* Windows 11 (Windows detected)
+
+**NOTE:** It has not been tested on macOS
+
+You can send a PR if you manage to detect other systems.
+
+![Windows](https://github.com/joelsernamoreno/ESP32Sx-DetectOS/blob/main/images/windows.jpg)
+
+![Linux](https://github.com/joelsernamoreno/ESP32Sx-DetectOS/blob/main/images/linux.png)
